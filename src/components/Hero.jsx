0a38f4b50350b42a0671f5926d409a953602a394
charlenebatiste <batistecharlene@gmail.com>
2021-07-26 13:57:25 -0400

    installed bulma and style nav and some of hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,26 @@
+import "bulma/css/bulma.min.css";
+import "../css/Hero.css";
+
 function Hero() {
 	return (
 		<div className="HeroSection">
-			<article>
-				<h1>Make the First Move</h1>
-				<p>
-					Start meeting new people in
-					your area! If you already have
-					an account, sign in to use
-					Bumble on the web.
-				</p>
-			</article>
-			<div>
-				<button>Join</button>
-				<button>Sign In</button>
+			<div class="card">
+				<div class="card-content">
+					<p class="title">
+						Make the first move
+					</p>
+					<div class="content">
+						Start meeting new people
+						in your area! If you
+						already have an account,
+						sign in to use Bumble on
+						the web..
+					</div>
+					<div>
+						<button>Join</button>
+						<button>Sign In</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,86 @@
+import "bulma/css/bulma.min.css";
+import "../css/Nav.css";
+
 function Nav() {
 	return (
 		<div className="navSection">
-			<div class="logoWrapper">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Bumble_logo_with_wordmark.svg" />
-			</div>
-			<div class="navigation">
-				<div class="itemWrapper">
-					<div class="nav-item">
-						<a href="#">The App</a>
-					</div>
-					<div class="nav-item">
-						<a href="#">Gift Cards</a>
-					</div>
-					<div class="nav-item">
-						<a href="#">
+			<nav
+				class="navbar"
+				role="navigation"
+				aria-label="main navigation"
+			>
+				<div class="navbar-brand">
+					<a class="navbar-item">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Bumble_logo_with_wordmark.svg"
+							width="188px"
+							height="32px"
+						/>
+					</a>
+
+					<a
+						role="button"
+						class="navbar-burger"
+						aria-label="menu"
+						aria-expanded="false"
+						data-target="navbarBasicExample"
+					>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+
+				<div
+					id="navbarBasicExample"
+					class="navbar-menu"
+				>
+					<div class="navbar-end">
+						<div class="navbar-item has-dropdown is-hoverable">
+							<a class="navbar-item">
+								The App
+							</a>
+
+							<div class="navbar-dropdown">
+								<a class="navbar-item">
+									DATE
+								</a>
+								<a class="navbar-item">
+									BFF
+								</a>
+								<a class="navbar-item">
+									BIZZ
+								</a>
+							</div>
+						</div>
+
+						<div class="navbar-item has-dropdown is-hoverable">
+							<a class="navbar-item">
+								Gift Cards
+							</a>
+
+							<div class="navbar-dropdown">
+								<a class="navbar-item">
+									Redeem
+								</a>
+								<a class="navbar-item">
+									Buy
+								</a>
+							</div>
+						</div>
+
+						<a class="navbar-item">
 							Ambassadors
 						</a>
-					</div>
-					<div class="nav-item">
-						<a href="#">The Buzz</a>
-					</div>
-					<div class="nav-item">
-						<a href="#">About</a>
+						<a class="navbar-item">
+							The Buzz
+						</a>
+						<a class="navbar-item">
+							About
+						</a>
 					</div>
 				</div>
-			</div>
+			</nav>
 		</div>
 	);
 }
